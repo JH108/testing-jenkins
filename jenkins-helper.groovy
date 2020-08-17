faff4ba20db68f06d088c18f0722a99f287ad0e8
Jesse Hill <jsnahill@gmail.com>
2020-08-17 15:40:45 -0500

    Only get subset of builds

--- jenkins-helper.groovy
+++ jenkins-helper.groovy
@@ -3,7 +3,8 @@ def call() {
 	def job = Jenkins.instance.getItemByFullName(currentBuild.projectName)
 	if (job != null) {
 		echo "Found the job! ${currentBuild.projectName}"
-		def builds = job.getBuilds()
+		def builds = job.getBuilds().byTimestamp(new Date(0).getTime(), new Date().minus(1).getTime())
+		echo "Number of builds: ${builds.length()}"
 		builds.each { build ->
 			def buildName = build.getDisplayName()
 			echo "Examining build: ${buildName}"
